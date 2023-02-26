2113025be9636181c8a32d38196a79b9ead72f4d
Bannikov Andrey <andrey27x777@gmail.com>
2023-02-26 05:49:32 +0200

    Tested os

--- index.py
+++ index.py
@@ -23,34 +23,40 @@ import os
 # available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
 # print(available_drives)
 
-drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
-print(os.getcwd())
-print(drives)
-
 from tkinter import *
 from tkinter import ttk
  
  
-# удаление выделенного элемента
 def select():
+    global cur_dir
+
     selection = file_listbox.curselection()
-    # мы можем получить удаляемый элемент по индексу
-    selected_language = file_listbox.get(selection[0])
-    # print(selected_language)
-    # print(file_listbox.size())
-    for root, dirs, files in os.walk(cur_dir):
-        # print(root)
-        for _dir in dirs:
-            print(_dir)
-    
-        for _file in files:
-            print(_file)
-    
- 
-# добавление нового элемента
-def add():
-    new_language = language_entry.get()
-    file_listbox.insert(0, new_language)
+    selected = file_listbox.get(selection[0])
+
+    print(selected)
+
+    pre_list = []
+
+    if selected in drives:
+        selected += '/'
+        cur_dir = os.path.abspath(selected)
+    else:
+        cur_dir = os.path.abspath(cur_dir + '/' + selected)
+
+    print(cur_dir)
+
+    pre_list.append('..')
+    temp_list = os.listdir(cur_dir)
+    cur_list = pre_list + temp_list
+    file_listbox.delete(0, END)
+    i = 0
+
+    for item in cur_list:
+        if os.path.isdir(os.path.abspath(cur_dir + '/' + item)):
+            i += 1
+            file_listbox.insert(i, item)
+
+    print(cur_list)
  
 root = Tk()
 root.title("METANIT.COM")
@@ -64,11 +70,14 @@ root.rowconfigure(index=2, weight=1)
 # текстовое поле и кнопка для добавления в список
 language_entry = ttk.Entry()
 language_entry.grid(column=0, row=0, padx=6, pady=6, sticky=EW)
-ttk.Button(text="Добавить", command=add).grid(column=1, row=0, padx=6, pady=6)
+# ttk.Button(text="Добавить", command=add).grid(column=1, row=0, padx=6, pady=6)
  
+drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
+
 # создаем список
-cur_dir = os.getcwd()
-cur_list = os.listdir(cur_dir)
+cur_dir = ''
+# cur_list = os.listdir(cur_dir)
+cur_list = drives
 list_var = Variable(value=cur_list)
 file_listbox = Listbox(listvariable=list_var)
 file_listbox.grid(row=1, column=0, columnspan=2, sticky=EW, padx=5, pady=5)
