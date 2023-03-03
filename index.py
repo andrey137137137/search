0c072c8e4b9c871724346fb6cebb72dd73ba2ca1
Andrey <andrey27x777@gmail.com>
2023-03-03 09:23:20 +0200

    Adding file search

--- index.py
+++ index.py
@@ -1,28 +1,4 @@
-# import win32api
-#
-# drives = win32api.GetLogicalDriveStrings()
-# drives = drives.split('\000')[:-1]
-# print(drives)
-
 import os
-# import string
-# from ctypes import windll
-#
-# def get_drives():
-#     drives = []
-#     bitmask = windll.kernel32.GetLogicalDrives()
-#     for letter in string.ascii_uppercase:
-#         if bitmask & 1:
-#             drives.append(letter)
-#         bitmask >>= 1
-#     return drives
-#
-# if (name == 'main'):
-#     print(get_drives())     # On my PC, this prints ['A', 'C', 'D', 'F', 'H']
-
-# available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
-# print(available_drives)
-
 from tkinter import *
 from tkinter import ttk
 
@@ -30,10 +6,48 @@ from tkinter import ttk
 def get_abspath(dir, name = ''):
     return os.path.abspath(dir + '/' + name)
 
+def search_file(path, name):
+    for item in os.listdir(path):
+        if item == name and os.path.isfile(item):
+            print('Name: ', name)
+            print('Size: ', os.path.getsize(item))
+            print('Path: ', os.path.abspath(item))
+            return
+        search_file(os.path.abspath(item), name)
+
+def get_file_selected():
+    selection = file_listbox.curselection()
+    return file_listbox.get(selection[0])
+
+def search():
+    name = file_entry.get()
+
+    if name == '':
+        return
+
+    selected = get_file_selected()
+    search_file(set_cur_dir(selected), name)
+
 def add(index, name):
     index += 1
     file_listbox.insert(index, name)
     return index
+
+def set_cur_dir(dir):
+    global cur_dir
+
+    if dir in drives:
+        cur_dir = get_abspath(dir)
+    elif len(cur_dir) == 3 and dir == '..':
+        cur_dir = ''
+    else:
+        path = get_abspath(cur_dir, dir)
+        if not os.path.isdir(path):
+            if os.path.isfile(path):
+                print(os.path.getsize(path))
+            return False
+        cur_dir = path
+    return True
 
 def set_cur_list():
     global cur_list
@@ -44,24 +58,11 @@ def set_cur_list():
         cur_list = ['..'] + os.listdir(cur_dir)
 
 def select():
-    global cur_dir
-
-    selection = file_listbox.curselection()
-    selected = file_listbox.get(selection[0])
-
+    selected = get_file_selected()
     print(selected)
 
-    if selected in drives:
-        cur_dir = get_abspath(selected)
-    elif len(cur_dir) == 3 and selected == '..':
-        cur_dir = ''
-    else:
-        path = get_abspath(cur_dir, selected)
-        if not os.path.isdir(path):
-            if os.path.isfile(path):
-                print(os.path.getsize(path))
-            return
-        cur_dir = path
+    if not set_cur_dir(selected):
+        return
 
     print(cur_dir)
 
@@ -77,7 +78,7 @@ def select():
  
 root = Tk()
 root.title("METANIT.COM")
-root.geometry("300x250")
+root.geometry("700x500")
 root.columnconfigure(index=0, weight=4)
 root.columnconfigure(index=1, weight=1)
 root.rowconfigure(index=0, weight=1)
@@ -85,8 +86,8 @@ root.rowconfigure(index=1, weight=3)
 root.rowconfigure(index=2, weight=1)
  
 # текстовое поле и кнопка для добавления в список
-language_entry = ttk.Entry()
-language_entry.grid(column=0, row=0, padx=6, pady=6, sticky=EW)
+file_entry = ttk.Entry()
+file_entry.grid(column=0, row=0, padx=6, pady=6, sticky=EW)
 # ttk.Button(text="Добавить", command=add).grid(column=1, row=0, padx=6, pady=6)
  
 drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
@@ -103,5 +104,18 @@ file_listbox.grid(row=1, column=0, columnspan=2, sticky=EW, padx=5, pady=5)
 # file_listbox.insert(END, "C#")
  
 ttk.Button(text="Перейти в папку", command=select).grid(row=2, column=1, padx=5, pady=5)
+
+tree = ttk.Treeview()
+# установка заголовка
+tree.heading("#0", text="Отделы", anchor=NW)
+tree.grid(row=1, column=2)
  
+tree.insert("", END, iid=1, text="Административный отдел", open=True)
+tree.insert("", END, iid=2, text="IT-отдел")
+tree.insert("", END, iid=3, text="Отдел продаж")
+ 
+tree.insert(1, index=END, text="Tom")
+tree.insert(2, index=END, text="Bob")
+tree.insert(2, index=END, text="Sam")
+
 root.mainloop()
