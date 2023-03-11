6ec540d5d8e2abfa7c39cd7a926a7aa1c83a9c6c
Bannikov Andrey <andrey27x777@gmail.com>
2023-03-11 10:34:05 +0200

    Added displaying path

--- index.py
+++ index.py
@@ -7,17 +7,43 @@ from tkinter import filedialog as fd
 
 def show_message(str, type = 'w'):
     if type == 'i':
-        return messagebox.showinfo(str)
+        return messagebox.showinfo('Сообщение', str)
     if type == 'w':
-        return messagebox.showwarning(str)
-    messagebox.showerror(str)
+        return messagebox.showwarning('Предупреждение', str)
+    messagebox.showerror('Ошибка', str)
 
 def select_file_name():
-    global file_name
-    file_name = fd.askopenfilename()
+    global file_name, file_size, file_mtime
+    temp = fd.askopenfilename()
+    file_name = os.path.basename(temp)
+    file_size = os.path.getsize(temp)
+    file_mtime = os.path.getmtime(temp)
 
 def get_abspath(dir, name = ''):
-    return os.path.abspath(dir + '/' + name)
+    return os.path.abspath(os.path.join(dir, name))
+
+def add_to_tree(index, text):
+    if index == 0:
+        id = ''
+        iid = 1
+    else:
+        id = index
+        iid = index + 1
+    tree.insert(parent=id, index=END, iid=iid, text=text, open=True)
+
+def build_tree(path):
+    parts = path.split('\\')
+    print(len(parts))
+
+    for item in tree.get_children(''):
+        tree.delete(item)
+
+    add_to_tree(0, parts[0])
+
+    i = 1
+    while i < len(parts):
+        add_to_tree(i, parts[i])
+        i += 1
 
 def search_file(root, name):
     global deep_counter
@@ -30,10 +56,13 @@ def search_file(root, name):
             path = get_abspath(root, item)
             print(path)
             print(item)
-            if item == name and os.path.isfile(path):
+            print(name)
+            if item == name and os.path.isfile(path) and file_size == os.path.getsize(path) and file_mtime == os.path.getmtime(path):
                 print('Name: ', name)
                 print('Size: ', os.path.getsize(path))
                 print('Path: ', os.path.abspath(path))
+                show_message('Name: ' + name, 'i')
+                build_tree(path)
                 deep_counter = deep
                 return
             if os.path.isdir(path):
@@ -59,12 +88,12 @@ def search():
         show_message('Выберите файл')
         return
 
-    selected = get_file_selected()
+    # selected = get_file_selected()
 
-    if not selected:
-        return
+    # if not selected:
+    #     return
 
-    set_cur_dir(selected)
+    # set_cur_dir(selected)
 
     if not cur_dir:
         show_message('Директория не выбрана')
@@ -92,6 +121,11 @@ def get_path(root, dir):
 
     return path
 
+def set_label_value(path):
+    global label_value, cur_dir
+    cur_dir = path
+    label_value.set(path)
+
 def set_cur_dir(dir):
     global cur_dir, prev_cur_dir
 
@@ -114,7 +148,8 @@ def set_cur_dir(dir):
         return False
 
     prev_cur_dir = cur_dir
-    cur_dir = path
+    # cur_dir = path
+    set_label_value(path)
     return True
 
 def set_cur_list():
@@ -128,7 +163,8 @@ def set_cur_list():
             cur_list = ['..'] + os.listdir(cur_dir)
         except:
             show_message('Невозможно открыть директорию: ' + cur_dir, 'e')
-            cur_dir = prev_cur_dir
+            # cur_dir = prev_cur_dir
+            set_label_value(prev_cur_dir)
             cur_list = prev_cur_list
 
 def select():
@@ -153,10 +189,11 @@ def select():
 root = Tk()
 root.title("METANIT.COM")
 root.geometry("700x500")
-root.columnconfigure(index=0, weight=4)
-root.columnconfigure(index=1, weight=1)
+root.columnconfigure(index=0, weight=0)
+root.columnconfigure(index=1, weight=0)
+root.columnconfigure(index=2, weight=3)
 root.rowconfigure(index=0, weight=1)
-root.rowconfigure(index=1, weight=3)
+root.rowconfigure(index=1, weight=1)
 root.rowconfigure(index=2, weight=1)
  
 # текстовое поле и кнопка для добавления в список
@@ -168,6 +205,7 @@ deep_counter = 0
 drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
 
 # создаем список
+file_size = 0
 cur_dir = prev_cur_dir = file_name = ''
 cur_list = prev_cur_list = drives
 list_var = Variable(value=cur_list)
@@ -177,13 +215,15 @@ file_listbox.grid(row=1, column=0, columnspan=2, sticky=EW, padx=5, pady=5)
 # добавляем в список начальные элементы
 # file_listbox.insert(END, "Python")
 # file_listbox.insert(END, "C#")
- 
+
+label_value = StringVar(value=cur_dir)
+ttk.Label(textvariable=label_value).grid(row=0, column=2)
 ttk.Button(text="Перейти в папку", command=select).grid(row=2, column=1, padx=5, pady=5)
 ttk.Button(text="Найти", command=search).grid(row=0, column=1, padx=5, pady=5)
 
 tree = ttk.Treeview()
 # установка заголовка
-tree.heading("#0", text="Отделы", anchor=NW)
+# tree.heading("#0", text="Отделы", anchor=NW)
 tree.grid(row=1, column=2)
  
 tree.insert("", END, iid=1, text="Административный отдел", open=True)
